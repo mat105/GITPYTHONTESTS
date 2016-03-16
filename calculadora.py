addd258c5fe74aa375b363d8e5d3abd3fe5d8e5c
Mat94 <mat.campos@yahoo.com.ar>
2016-03-16 01:36:25 -0300

    Primera version: SUMA/RESTA

---
+++ calculadora.py
@@ -0,0 +1,39 @@
+
+
+class Calculadora:
+
+    def sumar(self, a, b):
+        return a+b
+
+    def restar(self, a, b):
+        return a-b
+
+    def dale(self):
+        seguir=True
+        
+        while(seguir):
+            cmd = input("Comando (SUMAR|RESTAR|SALIR) (PARAMETRO1 PARAMETRO2)")
+
+            args = cmd.split(" ")
+
+            if(args[0]=="SALIR"):
+                seguir=False
+            elif(args[0]=="SUMAR" and len(args) == 3):
+                print( self.sumar( int(args[1]), int(args[2]) ) )
+            elif(args[0]=="RESTAR" and len(args) == 3):
+                print( self.restar( int(args[1]), int(args[2]) ) )
+            else:
+                print("Error, commando erroneo")
+        
+
+    def __init__(self):
+        self.dale()
+
+
+
+def main():
+    Calculadora()
+
+
+
+main()
