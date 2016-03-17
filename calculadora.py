10d7da43f83d199fe131ce10185327326198a79a
Eric <lozaeric@gmail.com>
2016-03-17 14:47:06 -0300

    agregado dividir y multiplicar

--- calculadora.py
+++ calculadora.py
@@ -7,13 +7,18 @@ class Calculadora:
 
     def restar(self, a, b):
         return a-b
+        
+    def multiplicar (self, a, b):
+        return a*b
 
+    def dividir (self, a, b):
+        return a/b
+        
     def dale(self):
         seguir=True
         
         while(seguir):
-            cmd = input("Comando (SUMAR|RESTAR|SALIR) (PARAMETRO1 PARAMETRO2): ")
-
+            cmd = input("Comando (SUMAR|RESTAR|MULTIPLICAR|DIVIDIR|SALIR) (PARAMETRO1 PARAMETRO2): ")
             args = cmd.split(" ")
 
             if(args[0]=="SALIR"):
@@ -22,6 +27,10 @@ class Calculadora:
                 print( self.sumar( int(args[1]), int(args[2]) ) )
             elif(args[0]=="RESTAR" and len(args) == 3):
                 print( self.restar( int(args[1]), int(args[2]) ) )
+            elif(args[0]=="MULTIPLICAR" and len(args) == 3):
+                print( self.multiplicar( int(args[1]), int(args[2]) ) )
+            elif(args[0]=="DIVIDIR" and len(args) == 3):
+                print( self.dividir(args[1]), int(args[2]) )               
             else:
                 print("Error, commando/parametros erroneos")
         
--- calcutest.py
+++ calcutest.py
@@ -9,6 +9,5 @@ class TestCalcu(unittest.TestCase):
         self.assertEqual( TestCalcu.Calcu.sumar(2,3), 5)
         self.assertEqual( TestCalcu.Calcu.restar(12,13), -1)
 
-
 if __name__ == '__main__':
     unittest.main()
